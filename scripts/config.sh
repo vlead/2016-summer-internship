790b92ebf64d8e311e7545016fc95ce40596d048
ksripathi <kammari.sripathi@gmail.com>
2016-05-03 16:21:09 +0530

    Added automation scripts

---
+++ scripts/config.sh
@@ -0,0 +1,7 @@
+#### PROXY CONFIGURATION #####
+http_proxy="http://proxy.iiit.ac.in:8080"
+https_proxy="http://proxy.iiit.ac.in:8080"
+
+#### GIT CONFIGURATION #####
+git_username=username
+git_email=useremail
